174b7126a88d6217a478ede5235f66c31ed5306b
Emmanuel Alves <emmanuel.sevla@gmail.com>
2016-06-28 16:06:14 -0300

    Editado Classe Cliente

--- src/br/com/SistemaRestauranteBarPizzaria/model/Cliente.java
+++ src/br/com/SistemaRestauranteBarPizzaria/model/Cliente.java
@@ -1,35 +1,14 @@
 package br.com.SistemaRestauranteBarPizzaria.model;
 
-public class Cliente extends DadosCadastrais{
-	public int idPedido;
+public class Cliente{
+	public int idPedidoCliente;
 	public String sugestaoOuReclamacao;
 	
-	public void setIdPedido(String nome){
-		this.nome = nome;
+	public void setIdPedido(int idPedidoCliente){
+		this.idPedidoCliente = idPedidoCliente;
 	}
-	public void setNome(String nome){
-		this.nome = nome;
-	}
-	public void setTelefone(String telefone){
-		this.telefone = telefone;
-	}
-	public void setCep(String cep){
-		this.cep = cep;
-	}
-	public void setNumero(int numero){
-		this.numero = numero;
-	}
-	public void setBairro(String bairro){
-		this.bairro = bairro;
-	}
-	public void setComplemento(String complemento){
-		this.complemento = complemento;
-	}
-	public void setCidade(String cidade){
-		this.cidade = cidade;
-	}
-	public void setEstado(String estado){
-		this.estado = estado;
+	public int getIdPedido(){
+		return idPedidoCliente;
 	}
 	public String getSugestaoOuReclamacao() {
 		return sugestaoOuReclamacao;
